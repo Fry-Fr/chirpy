b9cc70fc7c4280c02746973571322dade9162617
Fry-Fr <jwalker.ry@gmail.com>
2025-12-05 16:21:13 -0500

    update email not null

--- sql/schema/00001_users.sql
+++ sql/schema/00001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  email VARCHAR(50)
+  email VARCHAR(50) NOT NULL
 );
 
 -- +goose Down
