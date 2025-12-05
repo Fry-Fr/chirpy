ce058533d3498c33aed1597ad03a45a4f7d169fe
Fry-Fr <jwalker.ry@gmail.com>
2025-12-05 02:57:35 -0500

    add connection to database

--- config/config.go
+++ config/config.go
@@ -1,9 +1,29 @@
 package config
 
 import (
+	"database/sql"
+	"os"
 	"sync/atomic"
+
+	"github.com/Fry-Fr/chirpy/internal/database"
 )
+
+type State struct {
+	ApiConfig *ApiConfig
+	DB        *database.Queries
+}
 
 type ApiConfig struct {
 	FileserverHits atomic.Int32
+}
+
+func (s *State) ConnectDatabase() error {
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return err
+	}
+	dbQueries := database.New(db)
+	s.DB = dbQueries
+	return nil
 }
--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Fry-Fr/chirpy/config"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	state := &config.ApiConfig{}
+	godotenv.Load()
+	state := &config.State{
+		ApiConfig: &config.ApiConfig{},
+	}
+	if err := state.ConnectDatabase(); err != nil {
+		log.Printf("Error ConnectDatabase: %v\n", err)
+	}
 
-	handlerFileServ := state.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+	handlerFileServ := state.ApiConfig.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /admin/reset", state.HandleMetricsReset)
-	mux.HandleFunc("GET /admin/metrics", state.HandleMetricsLoad)
+	mux.HandleFunc("POST /admin/reset", state.ApiConfig.HandleMetricsReset)
+	mux.HandleFunc("GET /admin/metrics", state.ApiConfig.HandleMetricsLoad)
 	mux.HandleFunc("GET /api/healthz", config.HandleHealthzStatus)
 
 	mux.HandleFunc("POST /api/validate_chirp", config.HandleValidateChirp)
