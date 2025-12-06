4034390fad6d80650e78ca2659168791dff205c7
Fry-Fr <jwalker.ry@gmail.com>
2025-12-05 21:39:47 -0500

    add query get chirps

--- config/handler.go
+++ config/handler.go
@@ -137,6 +137,32 @@ func (cfg *ApiConfig) HandleCreateChirp(w http.ResponseWriter, r *http.Request) 
 	respondWithJSON(w, http.StatusCreated, payload)
 }
 
+func (cfg *ApiConfig) HandleGetChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.DB.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	type resVars struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}
+	payload := make([]resVars, len(chirps))
+	for i, chirp := range chirps {
+		payload[i] = resVars{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		}
+	}
+	respondWithJSON(w, http.StatusOK, payload)
+}
+
 // Handler Helpers ...
 func isValidEmail(email string) bool {
 	_, err := mail.ParseAddress(email)
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -5,3 +5,7 @@ VALUES (
   $2
 )
 RETURNING *;
+
+-- name: GetChirps :many
+SELECT * FROM chirps
+ORDER BY created_at;
