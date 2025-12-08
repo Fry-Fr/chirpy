199ef09247c8cf6764aaadd9f6d458006750a786
Fry-Fr <jwalker.ry@gmail.com>
2025-12-08 00:56:34 -0500

    add user_id on delete cascade

--- sql/schema/00004_refresh_tokens.sql
+++ sql/schema/00004_refresh_tokens.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE refresh_tokens (
     token VARCHAR(255) NOT NULL PRIMARY KEY,
-    user_id UUID REFERENCES users(id) NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL,
